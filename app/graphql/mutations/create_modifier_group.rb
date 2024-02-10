f3bb9f2da10484dbdd00e4526f367ce8e877f805
Prima Aulia Gusta <prima@tablevibe.co>
2024-02-10 22:50:00 +0800

    create basic mutation for mod group

---
+++ app/graphql/mutations/create_modifier_group.rb
@@ -0,0 +1,28 @@
+module Mutations
+  class CreateModifierGroup < Mutations::BaseMutation
+    argument :label, String, required: true
+    argument :selection_required_min, Integer, required: false
+    argument :selection_required_max, Integer, required: false
+
+    field :modifier_group, Types::ModifierGroupType, null: false
+    field :errors, [String], null: false
+
+    def resolve(label:, selection_required_min: 1, selection_required_max: 1)
+      # Create a new Item instance using the provided arguments
+      modifier_group = ModifierGroup.new(label:, selection_required_min:, selection_required_max:)
+
+      # Save the item and handle errors
+      if modifier_group.save
+        {
+          modifier_group:,
+          errors: []
+        }
+      else
+        {
+          modifier_group: nil,
+          errors: mod_group.errors.full_messages
+        }
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_menu, mutation: Mutations::CreateMenu
     field :create_section, mutation: Mutations::CreateSection
+    field :create_modifier_group, mutation: Mutations::CreateModifierGroup
   end
 end
