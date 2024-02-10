3715b205903862302acb1afe345d6464ea51fc6f
Prima Aulia Gusta <prima@tablevibe.co>
2024-02-10 20:29:36 +0800

    remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  root 'graphql#playground'
-
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   post '/graphql', to: 'graphql#execute'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
