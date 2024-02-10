e33db3c1c9cfa0dc5936b9137ba0d1055d021969
Prima Aulia Gusta <prima@tablevibe.co>
2024-02-10 17:01:32 +0800

    setup heroku

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.2.2'
 
 gem 'graphql'
+gem 'pg'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.8'
@@ -12,7 +13,6 @@ gem 'rails', '~> 7.0.8'
 gem 'sprockets-rails'
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~> 1.4'
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem 'puma', '~> 5.0'
@@ -64,4 +64,4 @@ group :test do
 end
 
 gem 'factory_bot', '~> 6.4'
-gem "graphiql-rails", group: :development
+gem 'graphiql-rails', group: :development
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_02_08_151109) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "item_modifier_groups", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.integer "modifier_group_id", null: false
+    t.bigint "item_id", null: false
+    t.bigint "modifier_group_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_item_modifier_groups_on_item_id"
@@ -31,8 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_08_151109) do
   end
 
   create_table "menu_sections", force: :cascade do |t|
-    t.integer "menu_id", null: false
-    t.integer "section_id", null: false
+    t.bigint "menu_id", null: false
+    t.bigint "section_id", null: false
     t.integer "display_order", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -60,8 +63,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_08_151109) do
   end
 
   create_table "modifiers", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.integer "modifier_group_id", null: false
+    t.bigint "item_id", null: false
+    t.bigint "modifier_group_id", null: false
     t.integer "display_order", default: 0
     t.integer "default_quantity", default: 0
     t.float "price_override"
@@ -72,8 +75,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_08_151109) do
   end
 
   create_table "section_items", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.integer "section_id", null: false
+    t.bigint "item_id", null: false
+    t.bigint "section_id", null: false
     t.integer "display_order", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
