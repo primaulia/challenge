4a14d68607928c3e1a8f3625edbd8820fd1946b0
Prima Aulia Gusta <prima@tablevibe.co>
2024-02-20 17:02:10 +0800

    update how display order is being set

--- app/models/menu_section.rb
+++ app/models/menu_section.rb
@@ -5,4 +5,14 @@ class MenuSection < ApplicationRecord
   belongs_to :section, optional: true
 
   validates :section_id, uniqueness: { scope: :menu_id }
+
+  before_save :set_display_order
+
+  private
+
+  # update display_order based on the last section made for a menu
+  def set_display_order
+    previous_menu_section = self.class.where(menu:).order('id DESC').first
+    self.display_order = previous_menu_section.present? ? previous_menu_section.display_order + 1 : 0
+  end
 end
--- app/models/section_item.rb
+++ app/models/section_item.rb
@@ -5,4 +5,14 @@ class SectionItem < ApplicationRecord
   belongs_to :section, optional: true
 
   validates :section_id, uniqueness: { scope: :item_id }
+
+  before_save :set_display_order
+
+  private
+
+  # update display_order based on the last section made for a menu
+  def set_display_order
+    previous_section_item = self.class.where(section:).order('id DESC').first
+    self.display_order = previous_section_item.present? ? previous_section_item.display_order + 1 : 0
+  end
 end
